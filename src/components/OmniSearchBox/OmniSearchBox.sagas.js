f1cdb9dc5d3c38cd6907f98b8df23d4e6a863d85
Darío Macchi <dariomac@gmail.com>
2019-08-01 22:57:49 -0300

    Remove jspath

--- src/components/OmniSearchBox/OmniSearchBox.sagas.js
+++ src/components/OmniSearchBox/OmniSearchBox.sagas.js
@@ -1,6 +1,5 @@
 import { takeLatest, select, put, call } from 'redux-saga/effects';
 import { actions, SEARCH, selectors } from './OmniSearchBox.module';
-import JSPath from 'jspath';
 import jsonata from 'jsonata';
 
 export function * searchSaga(){
@@ -8,22 +7,17 @@ export function * searchSaga(){
 }
 
 function * search(action) {
-  const haystack = yield select((state) => state[action.payload.inventorySelector])
+  const haystack = yield select((state) => state[action.payload.inventorySelector]);
 
-  // .articles.laneid
-  // ..laneid
-  // .inventory[0]{.laneid=='Essay'}.title
-  
   // Jsonata
   // **[$contains(`Product Name`, 'Hat')].SKU ~> $sort(function($l, $r) {$l > $r})
   // **[$contains(`Product Name`, 'Hat')].[SKU, Price]
   // **[$contains(`Product Name`, 'Hat')].({'s': $.SKU, 'p': $.Price})^(>s)
-  
+
   // **[$contains(`laneid`, 'Essay')].({'title':$.title, 'position':$.position})
   console.log(action.payload.needle)
   let res;
   try {
-    //res = JSPath.apply(action.payload.needle, haystack);
     const expr = jsonata(action.payload.needle);
     res = expr.evaluate(haystack);
   }
